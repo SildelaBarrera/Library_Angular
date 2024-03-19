4f0eea163ed9780ccc0592fa29177fab43738213
SildelaBarrera <sil.delabarrera@hotmail.com>
2024-03-19 13:01:43 +0100

    Commit Library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FormRegisterComponent } from './components/form-register/form-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    HeaderComponent,
+    FooterComponent,
+    FormRegisterComponent,
   ],
   imports: [
     BrowserModule,
